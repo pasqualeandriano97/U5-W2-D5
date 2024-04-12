9b92da510a8d5688e190ce9ba1951bea8efef99a
pasqualeandriano97 <151465437+pasqualeandriano97@users.noreply.github.com>
2024-04-12 12:29:15 +0200

    exception handler e entità dispositivo CRUD quasi interamente integrata

---
+++ src/main/java/andrianopasquale97/U5W2D5/controllers/DispositivoController.java
@@ -0,0 +1,5 @@
+package andrianopasquale97.U5W2D5.controllers;
+
+public class DispositivoController {
+
+}
--- src/main/java/andrianopasquale97/U5W2D5/services/DispositivoService.java
+++ src/main/java/andrianopasquale97/U5W2D5/services/DispositivoService.java
@@ -2,11 +2,18 @@ package andrianopasquale97.U5W2D5.services;
 
 
 import andrianopasquale97.U5W2D5.entities.Dispositivo;
-import andrianopasquale97.U5W2D5.payloads.DipendenteDTO;
+import andrianopasquale97.U5W2D5.enums.stato;
+
+import andrianopasquale97.U5W2D5.exceptions.NotFoundException;
 import andrianopasquale97.U5W2D5.payloads.DispositivoDTO;
 import andrianopasquale97.U5W2D5.repositories.DispositivoDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import andrianopasquale97.U5W2D5.enums.tipologie;
 
 @Service
 public class DispositivoService {
@@ -15,10 +22,62 @@ public class DispositivoService {
 
     public DispositivoDTO save(DispositivoDTO newDispositivo) {
 
-        Dispositivo dispositivo = new Dispositivo(newDispositivo.tipologia(), newDispositivo.stato());
+        Dispositivo dispositivo = new Dispositivo(tipologie(newDispositivo.tipologia()), statodispositivo(newDispositivo.stato()));
         dispositivoDAO.save(dispositivo);
         return newDispositivo;
+    }
 
+    public Page<Dispositivo> getDispositivi(int page, int size, String sortBy) {
+        if(size > 100) size = 100;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return dispositivoDAO.findAll(pageable);
+    }
+
+
+    public Dispositivo getDispositivoById(int id) {
+        return dispositivoDAO.findById(id).orElseThrow(()-> new NotFoundException("Dispositivo non trovato"));
+    }
+
+    public void findByIdAndDelete(int id) {
+        Dispositivo found = this.getDispositivoById(id);
+        this.dispositivoDAO.delete(found);
+    }
+
+
+    public Dispositivo update(int id, DispositivoDTO newDispositivo) {
+        Dispositivo found = this.getDispositivoById(id);
+        found.setTipologia(tipologie(newDispositivo.tipologia()));
+        found.setStato(statodispositivo(newDispositivo.stato()));
+        return found;
+    }
+
+
+    private tipologie tipologie(String tipologia) {
+        switch (tipologia) {
+            case "cellulare":
+                return tipologie.CELLULARE;
+            case "laptop":
+                return tipologie.LAPTOP;
+            case "tablet":
+                return tipologie.TABLET;
+            default:
+                throw new IllegalArgumentException("tipologia non valida");
+
+        }
+    }
+    private stato statodispositivo(String statoD) {
+        switch (statoD) {
+            case "disponibile":
+                    return stato.DISPONIBILE;
+            case "assegnato":
+                return stato.ASSEGNATO;
+            case "in_manutenzione":
+                return stato.IN_MANUTENZIONE;
+            case "dismesso":
+                return stato.DISMESSO;
+            default:
+                throw new IllegalArgumentException("stato non valido");
+        }
     }
 
 }
